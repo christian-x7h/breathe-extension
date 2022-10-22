20dca8b76bddbcf3c682c4de9f97d4b7c0f62033
Christian <christian-x7h@outlook.com>
2022-10-22 16:21:52 -0600

    Finish animation, nav components, need to add counters, and dismiss/continue functionality

--- content-script.js
+++ content-script.js
@@ -3,17 +3,15 @@ setTimeout(() => {
     $('#myH1').text('Woah I have changed!');
 }, timerLength);
 
-var prefix = "extension://ihgeokjjnbmhbjojfgpigdflcjklplim/";
+const onViewLoad = () => {
+    $('#eBreathOverlay_slideIn').on('animationend', function(){
+        $('#eBreathOverlay_slideIn').css('display', 'none');
+        $('#eBody').addClass('eBody_postBreath');
+        $('#decisionNav').css('display', 'block');
+    });
+}
 
 $(document).ready(function() {
     // Append main extension div to body
-    var extensionOverlay = $('body').append('<div id="eBody"></div>');
-
-    $('#eBody').load(chrome.runtime.getURL('views/main.html'));
-
-    $('preloader').on("animationend", function(){
-        $(this).css('display', 'none');
-    });
+    var extensionOverlay = $('body').load(chrome.runtime.getURL('views/main.html'), onViewLoad);
 });
-
-
